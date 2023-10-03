c74873d6bc8488b8d0d5dcd2b36caf95c63224b3
Anoop <anoopparashar6@gmail.com>
2023-10-04 01:53:53 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,6 +7,7 @@ import { BsCurrencyBitcoin } from 'react-icons/bs'
 import { HiInformationCircle } from 'react-icons/hi'
 export default function Navbar(props) {
 
+  
   const [txt,setText]=useState("")
 
   
