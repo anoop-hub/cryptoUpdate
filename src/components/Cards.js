5cf70a1856451fca18d08541279ad5e2d5db0e28
Anoop <anoopparashar6@gmail.com>
2023-10-13 00:04:01 +0530

    Update Cards.js

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -2,6 +2,8 @@ import React from 'react'
 import "./CardsHome.css"
 const Cards = ({stats,title}) => {
   return (
+
+    
     
     <div>
       <div className="card text-center project-card-view" style={{float:'none',marginBottom:'5px'}} >
